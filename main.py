d02ccb06d109b190fa9ba8f39102f4bb1bc61c96
Vic <vitrexesp@gmail.com>
2023-01-22 16:04:41 +0100

    Firs commit

---
+++ main.py
@@ -0,0 +1,47 @@
+from fastapi import FastAPI, Request, Form
+from fastapi.templating import Jinja2Templates
+from movie_scraping_2 import shot_cafe_scraping, movie_scraping
+from random import choice
+
+app = FastAPI()
+INTENTOS = None
+FILM_NAME = None
+FILM_IMGS = None
+templates = Jinja2Templates(directory="templates/")
+
+
+
+@app.get('/')
+def read_form(request: Request):
+    global INTENTOS
+    INTENTOS = 0
+    global FILM_NAME
+    global FILM_IMGS
+    while True:
+        try:
+            FILM_NAME, FILM_IMGS  = choice([shot_cafe_scraping()])
+            break
+        except:
+            print("Error")
+    return templates.TemplateResponse('index.html', context={"request": request, 'img': choice(FILM_IMGS),
+                                                             'film_name': FILM_NAME})
+
+@app.post("/submit/")
+async def submit(request: Request, film_guess: str = Form(...)):
+    global INTENTOS
+    global FILM_NAME
+    global FILM_IMGS
+    
+    film_guess = film_guess.strip().lower()
+    if film_guess != FILM_NAME.lower():
+        INTENTOS += 1
+        if INTENTOS < 6:
+            return templates.TemplateResponse('index.html', context={"request": request, 'incorrect_guess': "Incorrecto!",
+                                                                    "img": choice(FILM_IMGS)})
+        return templates.TemplateResponse('index.html', context={"request": request, 'incorrect_guess': f"Has perdido! La película era: {FILM_NAME}",
+                                                                    "img": choice(FILM_IMGS), "desactiva": True})
+        
+    return templates.TemplateResponse('index.html', context={"request": request, 
+                                                             'correct_guess': f"Correcto, la película era: {FILM_NAME}!",
+                                                             "img": choice(FILM_IMGS), "desactiva": True})
+    
---
+++ movie_scraping_2.py
@@ -0,0 +1,64 @@
+import requests
+from random import randint, choice
+from urllib.parse import urljoin
+from scrapy import Selector
+import re
+
+def movie_scraping():
+    years = [
+        2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010,
+        2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020
+    ]
+
+    params = {
+        "year": choice(years),
+        "page": randint(1, 5)
+    }
+
+    base_url = "https://www.moviestillsdb.com/"
+    url = "https://www.moviestillsdb.com/movies"
+
+    response = requests.get(url=url, params=params)
+    data = Selector(response=response)
+
+    movies_url = [urljoin(base_url, movie_url) for movie_url in data.css("td.title a ::attr(href)").getall()]
+
+    url = choice(movies_url)
+    response = requests.get(url=url)
+    data = Selector(response=response)
+    urls = re.findall(r'https[^"]*\.jpg', data.css("gallery").get())
+
+    movie_images = [img.replace("\\", "") for img in urls if "500x" in img] 
+    movie_title = data.css("a[itemprop=url] ::text").get()
+    if len(movie_images) >= 5:
+        print(response.url)
+        return [movie_title, movie_images]
+    movie_scraping()
+    
+def shot_cafe_scraping():
+    
+    genres = ["action", "adventure", "comedy", "crime", "drama", "family",
+              "fantasy", "history", "horror", "lgtb", "mystery", "romance",
+              "science fiction", "thriller", "war", "western"]
+    
+    base_url = "https://shot.cafe/"
+    url = f"https://shot.cafe/genres/{choice(genres)}"
+    response = requests.get(url=url)
+    data = Selector(response=response)
+    
+    films = []
+    for film in data.css(".project-list-item"):
+        if int(film.css("span::text").get()) >= 5:
+            films.append(urljoin(base_url, film.css("a::attr(href)").get()))
+            
+    chosen_film = choice(films)
+    response = requests.get(url=chosen_film)
+    data = Selector(response=response)
+    
+    film_title = data.css("h4 a ::text").get()
+    film_images = data.css("img.box-img ::attr(src)").getall()
+    final_images = []
+    for img in film_images:
+        final_images.append(urljoin(base_url, img.replace("/t/", "/o/")))
+        
+    return [film_title, final_images]
